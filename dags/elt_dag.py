90fde08af806ddd3ae43b4cf4d68559022c59bc5
Guilherme Belisario <gbo2000@outlook.com>
2026-04-18 00:39:07 -0300

    adicao do utils do pipleine, ajustes no funcionamento da dag e finalizacao do esqueleto da bronze

--- dags/elt_dag.py
+++ dags/elt_dag.py
@@ -1,80 +1,86 @@
 from airflow.sdk import dag, task
 from airflow.sensors.filesystem import FileSensor
-from dateime import datetime
+from datetime import datetime
+from pathlib import Path
+from utils.constants import entidades
 import glob
+import duckdb
 import shutil
+from time import sleep
 
 
-@dag("event_driven_dag", start_date=datetime(2024, 1, 1), schedule=None)
+@dag("event_driven_dag", start_date=datetime.now(), schedule="*/1 * * * *")
 def processador():
 
     sensor_clientes = FileSensor(
-        task_id="sensor_de_arquivos",
-        filepath="data/bronze/clientes/*json",
+        task_id="sensor_de_arquivos_clientes",
+        filepath="data/landing/clientes/*.json",
+        fs_conn_id='fs_default',
         poke_interval=30,
     )
 
     sensor_contas = FileSensor(
-        task_id="sensor_de_arquivos",
-        filepath="data/bronze/contas/*.json",
+        task_id="sensor_de_arquivos_contas",
+        filepath="data/landing/contas/*.json",
+        fs_conn_id='fs_default',
         poke_interval=30,
     )
 
     sensor_cartoes = FileSensor(
-        task_id="sensor_de_arquivos",
-        filepath="data/bronze/cartoes/*.json",
+        task_id="sensor_de_arquivos_cartoes",
+        filepath="data/landing/cartoes/*.json",
+        fs_conn_id='fs_default',
         poke_interval=30,
     )
 
     sensor_pix = FileSensor(
-        task_id="sensor_de_arquivos",
-        filepath="data/bronze/transacoes_pix/*.json",
+        task_id="sensor_de_arquivos_pix",
+        filepath="data/landing/transacoes_pix/*.json",
+        fs_conn_id='fs_default',
         poke_interval=30,
     )
 
     sensor_transacoes_cartao = FileSensor(
-        task_id="sensor_de_arquivos",
-        filepath="data/bronze/transacoes_cartao/*.json",
+        task_id="sensor_de_arquivos_transacoes_cartao",
+        filepath="data/landing/transacoes_cartao/*.json",
+        fs_conn_id='fs_default',
         poke_interval=30,
     )
 
     @task
-    def processar_silver_padronizados():
+    def processar_bronze_padronizados():
+
+        sleep(5)
 
         con = duckdb.connect()
         DESTINO = "data/bronze"
-
-        entidades = [
-            "clientes",
-            "contas",
-            "cartoes",
-            "transacoes_pix",
-            "transacoes_cartao",
-        ]
-        Path("data/silver").mkdir(parents=True, exist_ok=True)
+        DESTINO_PROCESSADOS = "data/landing_processados"
 
         for entidade in entidades:
-            Path(f"data/silver/{entidade}").mkdir(parents=True, exist_ok=True)
+            Path(f"data/bronze/{entidade}").mkdir(parents=True, exist_ok=True)
 
-            query = f"""SELECT * FROM read_json_auto('data/landing/{entidade}/*.json' LIMIT 1)"""
+            query = f"""SELECT * FROM read_json_auto('data/landing/{entidade}/*.json') LIMIT 1"""
 
-            df = con.query(query).fetchdf()
+            df = con.query(query).fetchone()
 
             if df:
 
                 query = f"""
                     COPY (
                         SELECT * FROM read_json_auto('data/landing/{entidade}/*.json')
-                    ) TO 'data/silver/{entidade}/{entidade}_silver{(datetime.now()).strftime("%Y-%m-%d-%H-%M-%S")}.parquet' (FORMAT PARQUET, COMPRESSION ZSTD);
+                    ) TO 'data/bronze/{entidade}/{entidade}_bronze{(datetime.now()).strftime("%Y-%m-%d-%H-%M-%S")}.parquet' (FORMAT PARQUET, COMPRESSION ZSTD);
                 """
                 con.execute(query)
 
+                Path(f"{DESTINO_PROCESSADOS}").mkdir(parents=True, exist_ok=True)
+
                 for arquivo in glob.glob(f"data/landing/{entidade}/*.json"):
-                    Path(f"{DESTINO}/{entidade}").mkdir(parents=True, exist_ok=True)
+
+                    Path(f"{DESTINO_PROCESSADOS}/{entidade}").mkdir(parents=True, exist_ok=True)
                     try:
-                        shutil.move(arquivo, f"{DESTINO}/{entidade}/")
+                        shutil.move(arquivo, f"{DESTINO_PROCESSADOS}/{entidade}/")
                         print(
-                            f"Sucesso ao mover o arquivo: {arquivo} para {DESTINO}/{entidade}/"
+                            f"Sucesso ao mover o arquivo: {arquivo} para {DESTINO_PROCESSADOS}/{entidade}/"
                         )
                     except:
                         print(f"Erro ao mover arquivo {arquivo}")
@@ -82,36 +88,30 @@ def processador():
                 print(f"Nenhum arquivo encontrado na entidade {entidade}")
 
         con.close()
-        return "Camada Silver (Parquet) Processada com sucesso."
+        return "Camada bronze (Parquet) Processada com sucesso."
 
     @task
-    def processar_gold_agregacoes(status_validacao):
+    def processar_silver_agregacoes():
+
         con = duckdb.connect()
-        Path("data/gold/resumos").mkdir(parents=True, exist_ok=True)
 
-        # Cria uma tabela resumo (Gold) de transações PIX por tipo de chave
-        query_pix_resumo = """
-            COPY (
-                SELECT 
-                    data_processamento,
-                    tipo_chave,
-                    status,
-                    COUNT(id_transacao) as qtd_transacoes,
-                    SUM(valor) as valor_total
-                FROM read_parquet('data/silver/transacoes_pix/*.parquet')
-                GROUP BY 1, 2, 3
-            ) TO 'data/gold/resumos/pix_diario.parquet' (FORMAT PARQUET);
-        """
-        con.execute(query_pix_resumo)
+        for entidade in entidades:
 
-        # Outras agregações (exemplos: média de gastos com cartão de crédito, saldo de contas, etc)
+            Path(f"data/silver/{entidade}").mkdir(parents=True, exist_ok=True)
 
-        con.close()
-        return "Camada Gold gerada"
+            con.close()
+            return print("Arquivos padronizados!")
 
-    # Definindo as dependências (Ordem de Execução)
+    sensores = [
+        sensor_clientes,
+        sensor_contas,
+        sensor_cartoes,
+        sensor_pix,
+        sensor_transacoes_cartao,
+    ]
+
     (
-        processar_silver_padronizados()
-        >> validar_qualidade_silver()
-        >> processar_gold_agregacoes()
+        sensores >> processar_bronze_padronizados() >> processar_silver_agregacoes()
     )
+       
+dag_pipeline = processador()
